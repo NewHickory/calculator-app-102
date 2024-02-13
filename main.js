c49e59820663d067f29fa4f5c499a90c667d2601
New Hickory <andrew.r.spear@gmail.com>
2024-02-12 20:17:20 -0600

    fixed button version except for sign_change and percentage

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 // These variable hold the numbers we want to do operations on and the name of the operation we want to perform.
 // They are expected to change so we use the "let" keyword.
-let inputNum = null
+
+let currentInput = null
+
+let firstNum = null
 let secondNum = null
+
+let result = null
+
 let operation = null
 
-// this function takes in the number you type in the input field and saves it to the "firstNum" variable
-const saveInputNumber = (num) => {
-  inputNum = parseInt(num) 
-  console.log("inputNum: ", inputNum)
-  putInputInElement(inputNum)
-}
+// this function takes in the number button you push and concats it to the currentInput variable
+// if the decimal button is pushed a second time on the same number, it shows an error message
 
-// this function takes in the number you type in the 2nd input field and saves it to the "secondNum" variable
-const saveSecondNumber = (num) => {
-  // "parseInt" is a built in function in JS that converts a string/word into a number
-  secondNum = parseInt(num)
+const saveInput = (lastInput) => { 
+  if (currentInput === null) {
+    currentInput = lastInput
+  }
+  else if (currentInput.includes(".") && lastInput === ".") {
+    alert("You can't enter two decimal places in the same number.")
+  }
+  else {
+    currentInput += lastInput
+  }
+  console.log("lastInput: ", lastInput)
+  console.log("Type of lastInput: ", typeof(lastInput))
+  console.log("currentInput: ", currentInput)
+  console.log("Type of currentInput: ", typeof(currentInput))
+  putInputInElement(currentInput)
 }
 
 // this function takes in two argument/numbers and returns the sum of them
@@ -52,29 +65,44 @@ const modulus = (numA, numB) => {
 }
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
+// It also converts to currentInput variable to a number and saves it as a new variable named operand.
+// Then it set the currentInput variable to null again; now ready for new number input.
 const changeOperation = (chosenOperation) => {
+  
+  if (result === null) {
+    firstNum = Number(currentInput)
+    currentInput = null
+  }
+  else {
+    firstNum = Number(result)
+  }
+  
+  if (chosenOperation === "")
   operation = chosenOperation
-  // Use your Chrome Inspector Tool > Console Tab to see the "operation" that's logged
-  console.log(operation)
+  // Log the value and data type of the first operand value as well as the value of the operation variable.
+  console.log("firstNum: ", firstNum)
+  console.log("firstNum type: ", typeof(firstNum))
+  console.log("Chosen Operation: ", operation)
 }
 
 // Show last input number
-const putInputInElement = (inputNum) => {
+const putInputInElement = (currentInput) => {
   // access the DOM by writing "document" then use the method "getElementById" and pass it the id, "input".
-  document.getElementById("input").innerHTML += inputNum
+  document.getElementById("input").innerHTML = currentInput
 }
 
 // In order to show the user their results we have to access the DOM and stick in the value
 const putResultInElement = (operationResults) => {
   // access the DOM by writing "document" then use the method "getElementById" and pass it the id, "result".
-  document.getElementById("result").innerHTML = "Results: " + operationResults
-
-  // Remember, each element has built in properties like "innerHTML" which we can change to anything we like. 
-  // Here we give it a string: "Results: " and add the value of the operation to it.
+  document.getElementById("result").innerHTML = operationResults
 }
 
 // The function uses the value of "operation" variable to determine which operation function it should use on the number: add, subtract, multiply, divide, or modulus
 const equals = () => {
+  
+  secondNum = Number(currentInput)
+  currentInput = null
+  
   switch (operation) {
     case "addition":  putResultInElement(add(firstNum, secondNum)) 
     break;
@@ -88,4 +116,19 @@ const equals = () => {
     break;
     default: "Choose an operation"
   }
+
+  result = document.getElementById("result").innerHTML
+  console.log("Current Result: ", result)
 }
+
+const all_clear = () => {
+  putInputInElement(null)
+  putResultInElement(null)
+  firstNum = null
+  secondNum = null
+  currentInput = null
+  result = null
+}
+
+
+
